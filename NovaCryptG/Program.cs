b7ebeda2b0c68af79c68ea5af963885c29dcd8f6
xyz <ArithmeticLogic@users.noreply.github.com>
2026-05-07 18:19:17 +0100

    Security Fixes - Hard coded password removed - Database is no longer created in the case that it does not exist - Added try catch to test for database errors

--- NovaCryptG/Program.cs
+++ NovaCryptG/Program.cs
@@ -1,7 +1,7 @@
+using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using NovaCryptG.Components;
 using NovaCryptG.Data;
-using NovaCryptG.Models;
 using NovaCryptG.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,41 +16,29 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 // Scoped services
-builder.Services.AddScoped<CryptographyService>();
+builder.Services.AddScoped<EncryptionService>();
 builder.Services.AddScoped<FileStorageService>();
 builder.Services.AddScoped<AuthenticationService>();
 builder.Services.AddScoped<UserSessionService>();
 
-// --- SQLite database setup ---
-var dbFolder = Path.Combine(AppContext.BaseDirectory, "Data");
-Directory.CreateDirectory(dbFolder); // ensure folder exists
-var dbPath = Path.Combine(dbFolder, "LoginData.db");
+// SQLite database path
+// TODO: Maybe add this try catch to other code that also looks for database data.
+try
+{
+    var dbFolder = Path.Combine(AppContext.BaseDirectory, "Data");
+    var dbPath = Path.Combine(dbFolder, "LoginData.db");
 
 builder.Services.AddDbContextFactory<AppDbContext>(options =>
     options.UseSqlite($"Data Source={dbPath}"));
+}
+catch (SqliteException ex)
+{
+    Console.WriteLine("Sqlite connection could not be established.");
+    Console.WriteLine(ex);
+}
 
 // Build the app
 var app = builder.Build();
-
-// Create database table if it does not exist
-using (var scope = app.Services.CreateScope())
-{
-    var factory = scope.ServiceProvider.GetRequiredService<IDbContextFactory<AppDbContext>>();
-    using var db = factory.CreateDbContext();
-
-    db.Database.EnsureCreated();
-
-    if (!db.LoginCredentials.Any())
-    {
-        db.LoginCredentials.Add(new LoginCredential
-        {
-            UserName = "Admin",
-            UserPassword = BCrypt.Net.BCrypt.HashPassword("Admin"),
-            IsAdmin = true
-        });
-        db.SaveChanges();
-    }
-}
 
 // Production settings
 if (!app.Environment.IsDevelopment())
